56dd00696c362b51b0b786652b58527b8ba3c555
shiva-raj1918 <shiva8639577401@gmail.com>
2026-03-05 13:09:43 +0530

    first commit

---
+++ src/org/tns/accenture/c2tc/Constructordemo/BeforeConstructor.java
@@ -0,0 +1,19 @@
+package org.tns.accenture.c2tc.Constructordemo;
+class Myclass
+{
+	String color;
+	int cost;
+	String Brand;
+}
+
+public class BeforeConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Myclass obj=new Myclass();
+		System.out.println(obj.color + " " + obj.cost + "  " + obj.Brand);
+
+	}
+
+}
